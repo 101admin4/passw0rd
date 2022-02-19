27b385d7eea8fc53d51734b5ef9e1ab599d114be
101admin4 <100017904+101admin4@users.noreply.github.com>
2022-02-19 11:16:22 +0300

    Add files via upload

---
+++ Генерация паролей.ps1
@@ -0,0 +1,2 @@
+﻿$lengthPassword = Read-Host 'Введите длину пароля'
+get-random -count $lengthPassword -input (33..47 + 48..57 + 65..90 + 97..122) | foreach-object -begin {$pass = $null} -process {$pass += [char]$_} -end {$pass}
